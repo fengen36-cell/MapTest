d441c7abfdd4295273397b714283f7a218bc5a2a
fengen36-cell <fengen36@gmail.com>
2025-10-17 14:47:19 +0800

    Add files via upload

---
+++ tilesets/Tilesets and props Demo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Tilesets and props Demo" tilewidth="32" tileheight="32" tilecount="546" columns="26">
+ <image source="Tilesets and props Demo.png" width="832" height="672"/>
+</tileset>
