dc6d32aed6e154e47205c341d3d25c3c71dc0e04
fengen36-cell <fengen36@gmail.com>
2025-10-31 14:27:50 +0800

    Delete tilesets directory

--- tilesets/B32x32.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="B32x32" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="../../32x32/B32x32.png" width="512" height="512"/>
-</tileset>
